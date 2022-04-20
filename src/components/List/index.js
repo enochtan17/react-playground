4dda6b12743e350dcb6178865be15a19994aff3a
Enoch Tan <enochtan@umich.edu>
2022-04-20 00:57:56 -0400

    form

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -3,21 +3,10 @@ import React, { useState } from "react"
 function List({ breeds }) {
     const [display, setDisplay] = useState(false)
 
-    // let breedsObj = {}
-
     const listButton = () => {
         setDisplay(!display)
-
-        // fetch('https://dog.ceo/api/breeds/list/all')
-        //     .then(res => res.json())
-        //     .then(data => {
-        //         // console.log(data)
-        //         breedsObj = {
-        //             ...data.message
-        //         }
-        //     })
     }
-    // console.log(breedsObj)
+
 
     return (
         <>
