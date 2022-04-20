23833e6fe81a80fc368bd42814cc2e92777769cc
Enoch Tan <enochtan@umich.edu>
2022-04-20 00:58:07 -0400

    form2

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,8 +1,34 @@
-import React from "react"
+import React, { useState } from "react"
 
 function Form() {
+    const [answer, setAnswer] = useState('')
+    const [feedback, setFeedback] = useState('')
+
+    const submitForm = (e) => {
+        e.preventDefault()
+
+        if (parseInt(answer) !== 25) setFeedback('Wrong. Try again.')
+        else setFeedback('Correct. Nice job.')
+        setAnswer('')
+    }
+
     return (
         <>
+            <div>
+                Hello!
+            </div>
+            <form onSubmit={submitForm}>
+                <label>
+                    5x5:
+                </label>
+                <input
+                    type="text"
+                    placeholder="answer"
+                    value={answer}
+                    onChange={e => setAnswer(e.target.value)}
+                />
+            </form>
+            {feedback ? <p>{ feedback }</p> : ''}
         </>
     )
 }
